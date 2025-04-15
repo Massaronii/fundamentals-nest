31447a839103750d96b2bab8c973898839a4e818
Yago <yagosonicmassaroni@gmail.com>
2025-04-14 21:16:47 -0300

    feat: create question

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,17 +5,17 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { Env } from "src/env";
 import { z } from "zod";
 
-const tokenSchema = z.object({
+const tokenPayloadSchema = z.object({
     sub: z.string().uuid(),
 });
 
-type TokenSchema = z.infer<typeof tokenSchema>;
+export type UserPayload = z.infer<typeof tokenPayloadSchema>;
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) { 
+export class JwtStrategy extends PassportStrategy(Strategy) {
 
     constructor(config: ConfigService<Env, true>) {
-        const publicKey = config.get('JWT_PUBLIC_KEY', {infer: true});
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
 
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -24,7 +24,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         })
     }
 
-    async validate(payload: TokenSchema) {
-        return tokenSchema.parse(payload);
+    async validate(payload: UserPayload) {
+        return tokenPayloadSchema.parse(payload);
     }
 }
