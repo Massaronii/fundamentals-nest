8e5573477b9eceb7ed2b589dc076e2b54bf7f095
Yago <yagosonicmassaroni@gmail.com>
2025-04-18 20:39:58 -0300

    feat: cryptography gateway

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "@/infra/auth/jwt-auth-guard";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
-import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { number, z } from "zod";
+import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
+import { QuestionPresenter } from "../presenters/question-presenter";
 
 const pageQueryParamSchema = z.string()
     .optional()
@@ -18,22 +19,23 @@ type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
 @UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionController {
 
-    constructor(private prisma: PrismaService) { }
+    constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) { }
 
     @Get()
     async handle(
         @Query('page', queryValidationPipe) page: PageQueryParamSchema,
     ) {
-        const PERPAGE = 20
 
-        const questions = await this.prisma.question.findMany({
-            take: PERPAGE,
-            skip: (page - 1) * 1,
-            orderBy: {
-                createdAt: 'desc',
-            },
+        const result = await this.fetchRecentQuestions.execute({
+            page,
         });
 
-        return { questions };
+        if(result.isLeft()) {
+            throw new Error();
+        }
+
+        const questions = result.value.questions;
+
+        return { questions: questions.map(QuestionPresenter.toHTTP) };
     }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,9 +3,9 @@ import { CreateAccountController } from "./controllers/create-account-controller
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
 import { FetchRecentQuestionController } from "./controllers/fetch-recent-questions.controller";
-import { PrismaService } from "../database/prisma/prisma.service";
 import { DatabaseModule } from "../database/database.module";
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
+import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
 
 @Module({
     imports: [DatabaseModule],
@@ -16,7 +16,8 @@ import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/crea
         FetchRecentQuestionController
     ],
     providers: [
-        CreateQuestionUseCase
+        CreateQuestionUseCase,
+        FetchRecentQuestionsUseCase
     ],
 })
 export class HttpModule { }
