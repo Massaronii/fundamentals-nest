88506ee316a3b9c6f630c28dbf9c4e6a89ff98a5
Yago <yagosonicmassaroni@gmail.com>
2025-04-27 12:19:17 -0300

    feat: integration with r2

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -38,9 +38,11 @@ import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cas
 import { FetchAnswerCommentsController } from "./controllers/fetch-answer-comments-controller";
 import { FetchAnswerCommentUseCase } from "@/domain/forum/application/use-cases/fetch-answer-comments";
 import { UploadAttachmentController } from "./controllers/upload-attachment.controller";
+import { StorageModule } from "../storage/storage.module";
+import { UploadAndCreateAttachmentUseCase } from "@/domain/forum/application/use-cases/upload-and-create-attachment";
 
 @Module({
-    imports: [DatabaseModule, CryptographyModule],
+    imports: [DatabaseModule, CryptographyModule, StorageModule],
     controllers: [
         CreateAccountController,
         AuthenticateController,
@@ -80,7 +82,8 @@ import { UploadAttachmentController } from "./controllers/upload-attachment.cont
         CommentOnAnswerUseCase,
         DeleteAnswerCommentAnswerUseCase,
         FetchQuestionCommentsUseCase,
-        FetchAnswerCommentUseCase
+        FetchAnswerCommentUseCase,
+        UploadAndCreateAttachmentUseCase
     ],
 })
 export class HttpModule { }
