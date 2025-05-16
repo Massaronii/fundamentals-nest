2efb6ac5d5c4a72cc7fcc28d0f350b66d86f6657
Yago <yagosonicmassaroni@gmail.com>
2025-05-15 22:27:42 -0300

    feat: last adjust cache

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -103,7 +103,7 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
         if(cacheHit) {
             const cachedData = JSON.parse(cacheHit);
 
-            return cachedData;
+            return PrismaQuestionDetailsMapper.toDomain(cachedData); 
         }
 
         const question = await this.prisma.question.findUnique({
@@ -120,12 +120,12 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
             return null;
         }
 
-        const questionDetails = PrismaQuestionDetailsMapper.toDomain(question); 
-
         await this.cache.set(
             `question:${slug}:details`,
-            JSON.stringify(questionDetails),
+            JSON.stringify(question),
         );
+
+        const questionDetails = PrismaQuestionDetailsMapper.toDomain(question); 
 
         return questionDetails;
     }
